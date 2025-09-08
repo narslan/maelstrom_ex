9e00371732eae47942ce26db20a7a82d73191f7f
Newroz Arslan <newrozarslan@gmail.com>
2025-09-08 16:59:50 +0300

    Init layout

---
+++ lib/maelstrom.ex
@@ -0,0 +1,18 @@
+defmodule Maelstrom do
+  @moduledoc """
+  Documentation for `Maelstrom`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Maelstrom.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
